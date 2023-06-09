f8c6648efcf4fa44fabd5ed75bfd1d0417fedb53
Marilyn Elaine Flowers <60019181+mflowers1395@users.noreply.github.com>
2023-06-08 19:06:18 -0500

    Creating Routes and Applications

--- Elaine/settings.py
+++ Elaine/settings.py
@@ -12,8 +12,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+PROJECT_DIR = os.path.join(BASE_DIR, "Elaine")
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -54,7 +58,7 @@ ROOT_URLCONF = 'Elaine.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(PROJECT_DIR,"templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
