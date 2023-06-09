9eee5d22bb684525fc3b8101fc18f2b1b27fe055
Marilyn Elaine Flowers <60019181+mflowers1395@users.noreply.github.com>
2023-06-08 19:21:50 -0500

    Adding links and navbar

--- Elaine/urls.py
+++ Elaine/urls.py
@@ -21,5 +21,7 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name="index")
+    path('home/', views.index, name="index"),
+    path('about/', views.about, name="about"),
+    path('contact/', views.contact, name="contact")
 ]
